f773734e81e010298ea93229b709b916582d4fa0
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-05-26 16:22:47 -0400

    Added data table generation from BN

--- src/edu/pitt/isp/sverchkov/bn/BNUtils.java
+++ src/edu/pitt/isp/sverchkov/bn/BNUtils.java
@@ -4,6 +4,8 @@
  */
 package edu.pitt.isp.sverchkov.bn;
 
+import edu.pitt.isp.sverchkov.data.DataTable;
+import edu.pitt.isp.sverchkov.data.DataTableImpl;
 import java.util.*;
 
 /**
@@ -54,5 +56,45 @@ public class BNUtils {
         // Optional sanity check could go here
         
         return result;
+    }
+    
+    public static <N,V> DataTable<N,V> generate( final BayesNet<N,V> net, final int n ){
+        return generate( net, n, new Random() );
+    }
+    
+    public static <N,V> DataTable<N,V> generate( final BayesNet<N,V> net, final int n, Random random ){
+        
+        final List<N> variables = nodesInTopOrder( net );        
+        final int m = variables.size();
+        final DataTable<N,V> data = new DataTableImpl<>( variables );
+        
+        for( int i=0; i<n; i++ ){
+            List<V> row = new ArrayList<>(m);
+            Map<N,V> conditions = new HashMap<>();
+            for( N variable : variables ){
+                V value = sample( net, variable, conditions, random );
+                row.add( value );
+                conditions.put( variable, value );
+            }
+            data.addRow( row );
+        }
+        return data;
+    }
+    
+    public static <N,V> V sample( final BayesNet<N,V> net, final N node, final Map<N,V> conditions, Random random ){
+        
+        Map<N,V> parentAssignment = new HashMap<>( conditions );
+        parentAssignment.keySet().retainAll( net.parents(node) );
+        
+        double p = random.nextDouble();
+        for( V value : net.values(node) ){
+            final double q = net.probability( Collections.singletonMap( node, value), conditions);
+            if( p < q )
+                return value;
+            else
+                p -= q;
+        }
+        
+        return null;
     }
 }
