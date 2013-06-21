261443b5232ae36835591320185d7ff55231298b
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-06-20 21:26:03 -0400

    Added writer of data to csv

--- src/edu/pitt/isp/sverchkov/data/DataTools.java
+++ src/edu/pitt/isp/sverchkov/data/DataTools.java
@@ -4,9 +4,9 @@
  */
 package edu.pitt.isp.sverchkov.data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,15 +15,40 @@ import java.util.Scanner;
  */
 public class DataTools {
     
-    public final static String DELIMITER = " *, *";
+    public final static String NEWLINE = System.getProperty("line.separator");
+    public final static String DELIMITER_REGEX = " *, *";
+    public final static String DELIMITER = ", ";
             
     public static DataTable<String,String> dataTableFromFile( File file ) throws FileNotFoundException{
         DataTable<String,String> data = null;
         try ( Scanner in = new Scanner( file ) ){
-            data = new DataTableImpl<>( Arrays.asList( in.nextLine().trim().split(DELIMITER) ) );
+            data = new DataTableImpl<>( Arrays.asList( in.nextLine().trim().split(DELIMITER_REGEX) ) );
             while( in.hasNextLine() )
-                data.addRow( Arrays.asList( in.nextLine().trim().split(DELIMITER) ) );
+                data.addRow( Arrays.asList( in.nextLine().trim().split(DELIMITER_REGEX) ) );
         }
         return data;
+    }
+    
+    public static <Attribute,Value> void saveCSV( DataTable<Attribute,Value> data, File dest, boolean headers ) throws IOException{
+        try( BufferedWriter out = new BufferedWriter( new FileWriter( dest ) ) ){
+            
+            if( headers ){
+                String delim = "";
+                for( Attribute a : data.variables() ){
+                    out.append(delim).append( a.toString() );
+                    delim = DELIMITER;
+                }
+                out.append(NEWLINE);
+            }
+            
+            for( List<Value> row : data ){
+                String delim = "";
+                for( Value v : row ){
+                    out.append(delim).append( v.toString() );
+                    delim = DELIMITER;
+                }
+                out.append(NEWLINE);
+            }
+        }
     }
 }
